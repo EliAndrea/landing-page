dd1ee2ee6c3f1202690bb0bc9fc1b73b65b42d41
Vcfade <vcfade@gmail.com>
2019-04-16 16:07:38 +0000

    fix card

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
-//create your first component
 
 export const Card = props => {
 	return (
 		<div className="card">
-			<img src={props.imageUrl} className="card-img-top" alt="..." />
 			<div className="card-body">
-				<h5 className="card-title"> {props.cardTitle}</h5>
+				<p className="card-text">{props.content}</p>
+				<a href={props.url} className="btn btn-primary">
+					{props.label}
+				</a>
 			</div>
 		</div>
 	);
 };
 
 Card.propTypes = {
-	imageUrl: PropTypes.string,
-	cardTitle: PropTypes.string
+	content: PropTypes.string,
+	url: PropTypes.string,
+	label: PropTypes.string
 };
